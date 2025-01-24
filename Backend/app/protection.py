17b0b6ac11bdbb0269b159b02e8f3d466dbed5be
CraiteX x REDLITE <rlcraitex@gmail.com>
2025-01-24 17:53:24 +0300

    api_hash

---
+++ Backend/app/protection.py
@@ -0,0 +1,28 @@
+import string
+import random 
+import bcrypt
+
+SYMBOLS = string.ascii_letters + string.digits
+
+async def generate_key():
+    chars = [SYMBOLS[random.randint(0, len(SYMBOLS))] for x in range(len(SYMBOLS))]    
+    key = ''.join(chars)
+
+    return key 
+
+
+async def hash_password(password):
+
+    salt = bcrypt.gensalt()
+
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password.decode('utf-8')
+
+
+async def check_password(password, hashed_password):
+    
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
+        return True
+    
+    return False
