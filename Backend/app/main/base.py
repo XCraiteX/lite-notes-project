ea50f60c21ba4d7a1562270cf10e47def05bb3c8
CraiteX x REDLITE <rlcraitex@gmail.com>
2025-01-30 13:01:33 +0300

    dock

--- Backend/app/main/base.py
+++ Backend/app/main/base.py
@@ -15,6 +15,7 @@ engine = create_async_engine(url='postgresql+asyncpg://postgres:password@postgre
 session = async_sessionmaker(bind=engine, class_=AsyncSession)
 
 async def create_tables():
+    sleep(3)
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
